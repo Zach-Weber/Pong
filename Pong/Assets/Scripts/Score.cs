824a24472648e89fd0450f3a9b73ede0998106d9
Zach-Weber <zachary.weber@digipen.edu>
2019-11-13 17:32:16 -0800

    Added score sound player to score.cs

--- Pong/Assets/Scripts/Score.cs
+++ Pong/Assets/Scripts/Score.cs
@@ -10,11 +10,19 @@ public class Score : MonoBehaviour
     private int[] playerScores = new int[2];
     public Text ScoreDisplay1, ScoreDisplay2;
 
+    // Add the sound clip to this script
+    public AudioClip ScoreSound;
+
     // Start is called before the first frame update
     void Start()
     {
         playerScores[0] = 0;
         playerScores[1] = 0;
+
+        // Sets playonawake to false so the game doesnt accidentally play the sound
+        GetComponent<AudioSource>().playOnAwake = false;
+        // Sets the audio clip on the Audio Source component to the clip that was added to this script
+        GetComponent<AudioSource>().clip = sound;
     }
 
     // Update is called once per frame
@@ -28,6 +36,9 @@ public class Score : MonoBehaviour
     public void IncrementScore(int player)
     {
         playerScores[player]++;
+
+        // plays the score sound
+        GetComponent<AudioSource>().Play();
     }
 
     public void CheckForWin()
