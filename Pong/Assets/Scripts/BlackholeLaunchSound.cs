ec0059d29ee58f62f5210f1f8e7bd2387962709a
Zach-Weber <zachary.weber@digipen.edu>
2019-11-15 16:30:40 -0800

    Added BlackholeLaunchSound.cs (WIP)

---
+++ Pong/Assets/Scripts/BlackholeLaunchSound.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackholeLaunchSound : MonoBehaviour
+{
+    // Add the sound clip to this script
+    public AudioClip LaunchSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Sets playonawake to false so the game doesnt accidentally play the sound
+        GetComponent<AudioSource>().playOnAwake = false;
+        // Sets the audio clip on the Audio Source component to the clip that was added to this script
+        GetComponent<AudioSource>().clip = LaunchSound;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("Play Sound (D)");
+            GetComponent<AudioSource>().Play();
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            Debug.Log("Play Sound (Arrow)");
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
